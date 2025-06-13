a38f4ff44c9015753fce3083fcaf2d0f0e71c264
rafaelabednego123 <rafaelabednego123@gmail.com>
2025-06-13 19:35:41 +0000

    Seeder

--- database/seeders/KucingSeeder.php
+++ database/seeders/KucingSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Kucing;
 
 class KucingSeeder extends Seeder
 {
@@ -12,6 +13,24 @@ class KucingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $kucings = [
+            [
+                'id_user' => null,
+                'name' => "Kucing Oren",
+                'warna' => "Orange",
+                'rating' => "7",
+                'imageUrl' => "gambar/Kucing2.jpg",
+            ],
+            [
+                'id_user' => null,
+                'name' => "Kucing Hitam",
+                'warna' => "Hitam",
+                'rating' => "6",
+                'imageUrl' => "gambar/Kucing1.jpg",
+            ],
+        ];
+        foreach ($kucings as $value) {
+            Kucing::create($value);
+        }
     }
 }
